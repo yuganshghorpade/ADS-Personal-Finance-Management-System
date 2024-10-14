6fa6ffde27efe9ca2b295774f4082d85715e141f
yuganshghorpade <yuganshghorpade2005@gmail.com>
2024-10-15 00:07:35 +0530

    Budget

--- src/main/java/com/pfms/BudgetManager.java
+++ src/main/java/com/pfms/BudgetManager.java
@@ -1,9 +1,18 @@
 package com.pfms;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class BudgetManager {
     private HashMap<String, Double> budgetCategories;
+    public static String budgetId;
 
     public BudgetManager() {
         budgetCategories = new HashMap<>();
@@ -13,13 +22,51 @@ public class BudgetManager {
         budgetCategories.put(category, budgetAmount);
     }
 
+    public void submitBudget(String userId){
+        MongoDatabase db = MongoDBConnection.connectToDatabase(); // Connect to the database
+        MongoCollection<Document> collection = db.getCollection("budgets"); // Get the collection
+
+        // for (Map.Entry<String, Double> entry : budgetCategories.entrySet()) {
+        //     Document doc = new Document("category", entry.getKey())
+        //                     .append("budget", entry.getValue());
+        //     collection.insertOne(doc); // Insert the document into the collection
+        // }
+
+        Document userBudgetDocument = new Document("userId", new ObjectId(userId))
+                .append("categories", new Document());
+
+        for (Map.Entry<String, Double> entry : budgetCategories.entrySet()) {
+            // Add each category to the user's budget document
+            userBudgetDocument.get("categories", Document.class)
+                    .append(entry.getKey(), entry.getValue());
+        }
+
+        // Insert the user's budget document into the collection
+        collection.insertOne(userBudgetDocument);
+        System.out.println("Budget categories saved to MongoDB successfully.");
+    }
+
+    //TODO:
     public void updateCategory(String category, double expenseAmount) {
         if (budgetCategories.containsKey(category)) {
             budgetCategories.put(category, budgetCategories.get(category) - expenseAmount);
         }
     }
 
-    public double getRemainingBudget(String category) {
-        return budgetCategories.getOrDefault(category, 0.0);
+    // public void getRemainingBudget(String category) {
+    //     MongoDatabase db = MongoDBConnection.connectToDatabase(); // Connect to the database
+    //     MongoCollection<Document> collection = db.getCollection("budgets"); // Get the collection
+    //     Document budget = collection.find(Filters.and(
+    //             Filters.eq("_id", budgetId) // Note: Storing plain text passwords is not secure
+    //     )).first();
+    // }
+
+    public void fetchBudget(){
+        MongoDatabase db = MongoDBConnection.connectToDatabase(); // Connect to the database
+        MongoCollection<Document> collection = db.getCollection("budgets"); // Get the collection
+        Document budget = collection.find(Filters.and(
+                Filters.eq("_id", new ObjectId(BudgetManager.budgetId)) // Note: Storing plain text passwords is not secure
+        )).first();
+        System.out.println("budget"+budget);
     }
 }
--- src/main/java/com/pfms/User.java
+++ src/main/java/com/pfms/User.java
@@ -1,13 +1,17 @@
 package com.pfms;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
 public class User {
 
+    public static boolean loggedIn;
+    public static String userId;
     private String username;
     private String password;  // In practice, this should be hashed.
     // private MongoDBConnection mongoDBConnection;
@@ -15,6 +19,7 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.loggedIn = false;
         // this.mongoDBConnection = new MongoDBConnection();
     }
 
@@ -29,6 +34,7 @@ public class User {
 
     public void registerUser(String name, String password) {
         MongoDatabase db = MongoDBConnection.connectToDatabase();
+        BudgetManager budgetManager = new BudgetManager();
         MongoCollection<Document> collection = db.getCollection("users");
 
         // Create a new user document
@@ -38,6 +44,23 @@ public class User {
         // Insert the new user into the collection
         // usersCollection.insertOne(newUser);
         collection.insertOne(newUser);
+
+        budgetManager.addCategory("Rent", 50000.0);
+        budgetManager.addCategory("Emi", 30000.0);
+        budgetManager.addCategory("Loan", 10000.0);
+        String userId = newUser.getObjectId("_id").toString();
+        budgetManager.submitBudget(userId);
+
+        Document user = collection.find(Filters.eq("_id", new ObjectId(userId))).first();
+        if (user != null) {
+        // Add a new field "budgetId" to the document
+        collection.updateOne(Filters.eq("_id", new ObjectId(userId)), Updates.set("budgetId", new ObjectId(budgetManager.budgetId)));
+
+        System.out.println("Budget ID added to user: " + user.getString("name"));
+    } else {
+        // User not found
+        System.out.println("User not found with ID: " + userId);
+    }
         System.out.println("New user registered: " + name);
     }
 
@@ -50,10 +73,16 @@ public class User {
                 Filters.eq("password", password) // Note: Storing plain text passwords is not secure
         )).first(); // Fetch the first matching document
         System.out.println(user);
+        User.userId= user.getObjectId("_id").toString(); // Correct way to get the _id field
+        BudgetManager.budgetId = user.getObjectId("budgetId").toString();
+        // BudgetManager.budgetId = user.getObjectId("").toString();
+        System.out.println("heyyy theree");
         if (user == null) {
             System.out.println("Please register before login or check credentials");
-        }
+        }else{
         System.out.println("Successfully logged In");
+        User.loggedIn = true;
+        }
 
     }
 }
