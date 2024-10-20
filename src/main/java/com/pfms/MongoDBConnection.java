0b53b5fb8392dfd54795b03aad410b7a9bfb0139
yuganshghorpade <yuganshghorpade2005@gmail.com>
2024-10-20 16:45:24 +0530

    FINAL

--- src/main/java/com/pfms/MongoDBConnection.java
+++ src/main/java/com/pfms/MongoDBConnection.java
@@ -11,16 +11,11 @@ public class MongoDBConnection {
     // Connect to the MongoDB server
     public static MongoDatabase connectToDatabase() {
         // You can replace the connection string with your MongoDB connection URI
-        // String uri = "mongodb://localhost:27017";
         String uri = "mongodb+srv://yuganshghorpadeviit:WTsEmDUQI2MV8Mes@cluster0.5ogrr.mongodb.net";
         mongoClient = MongoClients.create(uri);
-        // System.out.println(mongoClient);
         return mongoClient.getDatabase("pfms");  // Replace with your database name
     }
 
-    // public MongoDatabase getDatabase() {
-    //     return database;
-    // }
     
     // Close the connection
     public static void closeConnection() {
